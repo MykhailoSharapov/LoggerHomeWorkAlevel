42d132d1e94250f5633d706d6f100bae56f1a71f
Mykhailo Sharapov <mikesharapov1@gmail.com>
2020-11-24 23:34:42 +0200

    Action Class Added

---
+++ LoggerHomeWorkAlevel/Actions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerHomeWorkAlevel
+{
+    class Actions
+    {
+        public void MethodFirst()
+        {
+            Logger.NewMessage(GlobalConstant.Info, "Start method:");
+        }
+        public void MethodSecond()
+        {
+            Logger.NewMessage(GlobalConstant.Warning, "Skipped logic in method:");
+        }
+        public void MethodThird()
+        {
+            throw new Exception("I broke a toilet");
+        }
+    }
+}
