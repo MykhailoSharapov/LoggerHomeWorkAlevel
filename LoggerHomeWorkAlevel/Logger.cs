49793168c9e24be68a1948622219aaf5c7297d2d
Mykhailo Sharapov <mikesharapov1@gmail.com>
2020-11-24 23:31:13 +0200

    Logger New Feathures

--- LoggerHomeWorkAlevel/Logger.cs
+++ LoggerHomeWorkAlevel/Logger.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace LoggerHomeWorkAlevel
 {
@@ -13,14 +14,33 @@ namespace LoggerHomeWorkAlevel
 
         }
         private static Logger instance;
-
+        private static List<string> LogMessages = new List<string>();
         public static Logger GetInstance()
         {
-            if(instance == null)
+            if (instance == null)
             {
                 instance = new Logger();
             }
             return instance;
+        }
+
+        public static string NewMessage(string MessageLevel, Exception ex)
+        {
+            string result = $"{MessageLevel}, stack trace: {ex.StackTrace}, error message: {ex.Message}";
+            LogMessages.Add(result);
+            return result;
+        }
+
+        public static string NewMessage(string MessageLevel, string Message)
+        {
+            string result = $"{MessageLevel}, Message: {Message}";
+            LogMessages.Add(result);
+            return result;
+        }
+
+        public static void SaveLogFile()
+        {
+            File.AppendAllLines(DateTime.Now.ToString() + ".txt", LogMessages);
         }
     }
 }
